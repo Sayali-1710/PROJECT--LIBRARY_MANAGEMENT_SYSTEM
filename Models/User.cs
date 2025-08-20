c6650941cf8a59bb713fd09e5374fd76e01d1c33
Sayali-1710 <sayalitodkar17102001@gmail.com>
2025-08-20 10:39:00 +0530

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace PROJECT__LIBRARY_MANAGEMENT_SYSTEM.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Name { get; set; } = " ";
+        public string Email { get; set; } = "  ";
+        public string Password { get; set; } = "  ";
+
+        public string Role { get; set; } = "Member";
+
+        public DateTime MembershipDate { get; set; } = DateTime.Now;
+    }
+}
